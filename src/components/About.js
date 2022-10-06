9f00220d43b5f05a5c64727d72dc73d7d3a96ff3
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-10-06 12:10:31 +0300

    updated app.js

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+
+import React from "react";
+
+function About() {
+  return (
+    <div className="about">
+      <h1>ABOUT</h1>
+      <p>
+        This is a  Cocktail recipe app where users get to serch fordrinks based on
+        different things. <br />
+        They can search based on the name of the meal or by alphabetical letter.
+        <br />
+        The user can then click on the Drink card to get to see the ingredients and preparation
+        .
+        <br />
+    
+      </p>
+    </div>
+  );
+}
+
+export default About;
