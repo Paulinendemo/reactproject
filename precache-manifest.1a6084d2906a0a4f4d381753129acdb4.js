89d1e6dd3e39d6d1849c333701ee8742e9f934b7
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-10-07 12:08:02 +0300

    Updates

---
+++ precache-manifest.1a6084d2906a0a4f4d381753129acdb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "134767d7fd61a6ec09d7",
+    "url": "/react-gh-pages/static/css/main.1902543c.chunk.css"
+  },
+  {
+    "revision": "134767d7fd61a6ec09d7",
+    "url": "/react-gh-pages/static/js/main.f52112c2.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "82ac499f3abc7faf0f64",
+    "url": "/react-gh-pages/static/js/2.96ec0c0a.chunk.js"
+  },
+  {
+    "revision": "277de2421a536b5fc755205c3e7e8eb6",
+    "url": "/react-gh-pages/index.html"
+  }
+];
